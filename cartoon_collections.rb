2615cdfdfc83dde453798c3e2889e7dec192e7db
Nicholas Walton <cactar65@gmail.com>
2019-10-10 21:14:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  
+  names.each_with_index do |index, dwarf|
+    puts "#{index dwarf}"
+  end
+  
 end
 
 def summon_captain_planet# code an argument here
